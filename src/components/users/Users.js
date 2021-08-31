4d47b1e9ec68e0685f2b437b034ff870c95576e7
deepak-likes-code <deepakkomm@gmail.com>
2021-08-31 16:01:21 +0530

    added spinner refactored code

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,17 +1,22 @@
-import React, { Component } from 'react'
+import React from 'react'
 import UserItem from './UserItem'
-class Users extends Component {
+import Spinner from "../layout/Spinner";
+import PropTypes from 'prop-types'
 
 
+const Users = ({ users, loading }) => {
 
-    render() {
+    if (loading) {
+
+        return <Spinner />
+
+    } else {
         return (
             <div style={userStyle}>
 
-                {this.props.users.map(user => (
+                {users.map(user => (
                     <UserItem key={user.id} user={user} />
                 ))}
-
             </div>
         )
     }
@@ -24,5 +29,9 @@ const userStyle = {
     padding: '1rem'
 }
 
+Users.propTypes = {
+    users: PropTypes.array.isRequired,
+    loading: PropTypes.bool.isRequired,
+}
 
 export default Users
